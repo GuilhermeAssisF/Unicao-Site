3522ff4fcbe6a71ffe3d3a5be0dff93494ab1ff4
Guilherme Assis <assisguilhermefernandes@gmail.com>
2025-07-14 23:53:36 -0300

    config token access vercel

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,7 +1,20 @@
-import { NextResponse } from "next/server";
-import { revalidateTag } from "next/cache";
+// src/app/api/revalidate/route.ts
 
-export async function POST() {
+import { revalidateTag } from "next/cache";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const secret = request.headers.get("authorization");
+
+  // Verifica se o token secreto foi enviado e se corresponde ao do ambiente
+  if (secret !== `Bearer ${process.env.PRISMIC_REVALIDATE_SECRET}`) {
+    return NextResponse.json(
+      { message: "Token de autorização inválido." },
+      { status: 401 }
+    );
+  }
+
+  // Se o token for válido, revalida o cache
   revalidateTag("prismic");
 
   return NextResponse.json({ revalidated: true, now: Date.now() });
